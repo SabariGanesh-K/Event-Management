b808ef0db111e020214a1085b2a4b6f5aa83ae67
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2023-02-23 16:11:35 +0530

    stable version

--- src/pages/AddMember.js
+++ src/pages/AddMember.js
@@ -29,12 +29,12 @@ export const AddMember = () => {
     }
   return (
     <div className='flex flex-row justify-center'>
-    <div className=' w-[90%]  md:w-[90%] flex flex-col justify-center'>
+    <div className='w-[90%]  md:w-[90%] flex flex-col justify-center'>
     <a href="/home" class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-white">
 <span class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
 <span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease"> &lt;=== Back to Admin Panel</span>
 </a>
-      <div className='text-3xl font-bold font-mono mt-6'>Add Participants</div>
+      <div className='text-3xl  font-bold font-mono mt-6'>Add Participants</div>
       <Snackbar open={EntityClipOpen} autoHideDuration={3000} onClose={handleEntityClipClose}>
         <Alert className='bg-white' onClose={handleEntityClipClose} severity="success" sx={{ width: '100%' ,color:'white',bgcolor:'green'}}>
           USER ADDED SUCCESSFULLY !!
